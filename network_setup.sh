a7fd93f1eb3f27ca1b8e6972ff990cab42aaeb8c
wojkra <102611904+wojkra@users.noreply.github.com>
2024-09-18 19:43:26 +0200

    Update network_setup.sh

--- network_setup.sh
+++ network_setup.sh
@@ -6,50 +6,52 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Konfiguracja interfejsów sieciowych
-echo "Konfiguracja interfejsów sieciowych..."
+echo "Konfiguracja przestrzeni nazw sieciowych i interfejsów..."
 
-# Konfiguracja ens37
-ip addr flush dev ens37
-ip addr add 192.168.81.1/24 dev ens37
-ip link set ens37 up
+# 1. Stworzenie dwóch przestrzeni nazw sieciowych
+ip netns add ns1
+ip netns add ns2
 
-# Konfiguracja ens38
-ip addr flush dev ens38
-ip addr add 192.168.2.1/24 dev ens38
-ip link set ens38 up
+# 2. Przeniesienie interfejsów fizycznych do odpowiednich przestrzeni nazw
+ip link set ens37 netns ns1
+ip link set ens38 netns ns2
 
-# Włączenie przekazywania IP
-echo "Włączanie przekazywania IP..."
-sysctl -w net.ipv4.ip_forward=1
+# 3. Konfiguracja interfejsu w przestrzeni nazw ns1 (ens37)
+ip netns exec ns1 ip addr flush dev ens37
+ip netns exec ns1 ip addr add 192.168.81.1/24 dev ens37
+ip netns exec ns1 ip link set ens37 up
+ip netns exec ns1 ip link set lo up
 
-# Upewnienie się, że przekazywanie IP jest włączone przy starcie systemu
-if ! grep -q "net.ipv4.ip_forward=1" /etc/sysctl.conf; then
-  echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-fi
+# 4. Konfiguracja interfejsu w przestrzeni nazw ns2 (ens38)
+ip netns exec ns2 ip addr flush dev ens38
+ip netns exec ns2 ip addr add 192.168.2.1/24 dev ens38
+ip netns exec ns2 ip link set ens38 up
+ip netns exec ns2 ip link set lo up
 
-# Dodanie tras routingu (nie jest konieczne w tym przypadku, ponieważ interfejsy są bezpośrednio podłączone)
+# 5. Umożliwienie komunikacji między przestrzeniami nazw (opcjonalnie)
+# Możemy utworzyć mostek sieciowy lub użyć pary interfejsów veth, ale ponieważ używamy fizycznych interfejsów,
+# ruch między nimi powinien przechodzić przez fizyczną sieć (vSwitch na ESXi)
 
-# Dodanie reguł iptables do przekazywania pakietów między interfejsami
-echo "Konfiguracja iptables..."
+# 6. Sprawdzenie stanu interfejsów w przestrzeniach nazw
+echo "Interfejsy w ns1:"
+ip netns exec ns1 ip addr show ens37
+echo ""
+echo "Interfejsy w ns2:"
+ip netns exec ns2 ip addr show ens38
+echo ""
 
-# Usunięcie istniejących reguł
-iptables -F
-iptables -t nat -F
-iptables -t mangle -F
-iptables -X
-
-# Ustawienie polityki akceptacji dla łańcuchów INPUT, FORWARD i OUTPUT
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-
-# Włączenie przekierowania pakietów między interfejsami
-iptables -A FORWARD -i ens37 -o ens38 -j ACCEPT
-iptables -A FORWARD -i ens38 -o ens37 -j ACCEPT
-
-# Maskarada (jeśli potrzebna)
-# Jeśli chcesz użyć NAT, odkomentuj poniższą linię
-# iptables -t nat -A POSTROUTING -o ens38 -j MASQUERADE
-
+# 7. Informacje dla użytkownika
 echo "Konfiguracja zakończona."
+echo ""
+echo "Aby przetestować połączenie, użyj następujących poleceń:"
+echo "Ping z ns1 do ns2:"
+echo "sudo ip netns exec ns1 ping 192.168.2.1"
+echo ""
+echo "Ping z ns2 do ns1:"
+echo "sudo ip netns exec ns2 ping 192.168.81.1"
+echo ""
+echo "Aby uruchomić serwer Modbus w ns2, użyj:"
+echo "sudo ip netns exec ns2 python3 modbus_server.py"
+echo ""
+echo "Aby uruchomić klienta Modbus w ns1, użyj:"
+echo "sudo ip netns exec ns1 python3 modbus_client.py"
