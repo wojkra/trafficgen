b114a9c70c4e496178652e41533609192094b92a
wojkra <102611904+wojkra@users.noreply.github.com>
2024-09-30 14:20:23 +0200

    Update symulacja_scada.sh

--- symulacja_scada.sh
+++ symulacja_scada.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
+set -x
 
-# Funkcja do czyszczenia konfiguracji przy wyjściu
 function cleanup {
     echo "Czyszczenie konfiguracji..."
-    # Zatrzymaj wszystkie procesy w przestrzeniach nazw
-    ip netns pids ns_server | xargs kill -9
-    ip netns pids ns_client_modbus | xargs kill -9
-    ip netns pids ns_client_s7 | xargs kill -9
+    ip netns pids ns_server | xargs kill -9 2>/dev/null
+    ip netns pids ns_client_modbus | xargs kill -9 2>/dev/null
+    ip netns pids ns_client_s7 | xargs kill -9 2>/dev/null
 
-    # Przywróć interfejsy fizyczne do domyślnej przestrzeni nazw
-    ip netns exec ns_server ip link set ens37 netns 1
-    ip link set ens37 up
-    ip addr flush dev ens37
+    ip netns exec ns_server ip link set ens37 netns 1 2>/dev/null
+    ip link set ens37 up 2>/dev/null
+    ip addr flush dev ens37 2>/dev/null
 
-    ip netns exec ns_client_modbus ip link set ens38 netns 1
-    ip link set ens38 up
-    ip addr flush dev ens38
+    ip netns exec ns_client_modbus ip link set ens38 netns 1 2>/dev/null
+    ip link set ens38 up 2>/dev/null
+    ip addr flush dev ens38 2>/dev/null
 
-    # Usuń most i interfejsy wirtualne
-    ip netns exec ns_client_modbus ip link set br0 down
-    ip netns exec ns_client_modbus brctl delbr br0
-    ip netns exec ns_client_modbus ip link delete veth_s7_br type veth
-    ip netns exec ns_client_s7 ip link delete veth_s7 type veth
+    ip netns exec ns_client_modbus ip link set br0 down 2>/dev/null
+    ip netns exec ns_client_modbus brctl delbr br0 2>/dev/null
 
-    # Usuń przestrzenie nazw
-    ip netns delete ns_server
-    ip netns delete ns_client_modbus
-    ip netns delete ns_client_s7
+    ip link delete veth_s7_br type veth 2>/dev/null
+    ip link delete veth_s7 type veth 2>/dev/null
+
+    ip netns delete ns_server 2>/dev/null
+    ip netns delete ns_client_modbus 2>/dev/null
+    ip netns delete ns_client_s7 2>/dev/null
 
     echo "Konfiguracja wyczyszczona."
 }
 
-# Ustawienie pułapki na wyjście ze skryptu
 trap cleanup EXIT
 
 # Tworzenie przestrzeni nazw
@@ -51,7 +47,11 @@ ip netns exec ns_server ip link set ens37 up
 ip netns exec ns_client_modbus ip link set ens38 up
 
 # Tworzenie pary veth dla klienta S7
-ip netns exec ns_client_modbus ip link add veth_s7_br type veth peer name veth_s7 netns ns_client_s7
+ip link add veth_s7_br type veth peer name veth_s7
+
+# Przeniesienie interfejsów do odpowiednich przestrzeni nazw
+ip link set veth_s7_br netns ns_client_modbus
+ip link set veth_s7 netns ns_client_s7
 
 # Konfiguracja interfejsu klienta S7
 ip netns exec ns_client_s7 ip addr add 192.168.82.20/24 dev veth_s7
@@ -68,13 +68,9 @@ ip netns exec ns_client_modbus ip link set veth_s7_br up
 # Przypisanie adresu IP do mostu
 ip netns exec ns_client_modbus ip addr add 192.168.82.10/24 dev br0
 
-# Dodanie trasy w ns_client_s7 (jeśli konieczne)
-# ip netns exec ns_client_s7 ip route add default via 192.168.82.10
-
 # Uruchomienie serwera Modbus w ns_server
-ip netns exec ns_server bash -c "
-pip3 install pymodbus
-python3 -c \"
+ip netns exec ns_server bash -c '
+python3 -c "
 from pymodbus.server.sync import StartTcpServer
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext, ModbusSequentialDataBlock
 store = ModbusSlaveContext(
@@ -83,53 +79,53 @@ store = ModbusSlaveContext(
     hr=ModbusSequentialDataBlock(0, [17]*100),
     ir=ModbusSequentialDataBlock(0, [17]*100))
 context = ModbusServerContext(slaves=store, single=True)
-StartTcpServer(context, address=('192.168.81.10', 502))
-\" &" &
+StartTcpServer(context, address=(\"192.168.81.10\", 502))
+" &' &
+echo "Serwer Modbus uruchomiony"
 
 # Uruchomienie serwera S7 w ns_server
-ip netns exec ns_server bash -c "
-pip3 install python-snap7
-python3 -c \"
+ip netns exec ns_server bash -c '
+python3 -c "
 import time
 from snap7.server import Server as S7Server
 server = S7Server()
 server.start(tcpport=102)
 while True:
     time.sleep(1)
-\" &" &
+" &' &
+echo "Serwer S7 uruchomiony"
 
 # Czekamy chwilę, aby serwery się uruchomiły
 sleep 5
 
 # Uruchomienie klienta Modbus w ns_client_modbus
-ip netns exec ns_client_modbus bash -c "
-pip3 install pymodbus
-python3 -c \"
+ip netns exec ns_client_modbus bash -c '
+python3 -c "
 import random
 import time
 from pymodbus.client.sync import ModbusTcpClient
-client = ModbusTcpClient('192.168.81.10', port=502)
+client = ModbusTcpClient(\"192.168.81.10\", port=502)
 client.connect()
 try:
     while True:
         value = random.randint(10, 20)
         client.write_register(1, value)
         response = client.read_holding_registers(1, 1)
-        print(f'Modbus Client: Wrote and Read Value {response.registers[0]}')
+        print(f\"Modbus Client: Wrote and Read Value {response.registers[0]}\")
         time.sleep(5)
 finally:
     client.close()
-\" &" &
+" &' &
+echo "Klient Modbus uruchomiony"
 
 # Uruchomienie klienta S7 w ns_client_s7
-ip netns exec ns_client_s7 bash -c "
-pip3 install python-snap7
-python3 -c \"
+ip netns exec ns_client_s7 bash -c '
+python3 -c "
 import time
 import random
 from snap7.client import Client as S7Client
 client = S7Client()
-client.connect('192.168.81.10', 0, 1, 102)
+client.connect(\"192.168.81.10\", 0, 1, 102)
 data = bytearray([0]*10)
 try:
     while True:
@@ -137,11 +133,12 @@ try:
         data[0] = value
         client.db_write(1, 0, data)
         read_data = client.db_read(1, 0, 1)
-        print(f'S7 Client: Wrote and Read Value {read_data[0]}')
+        print(f\"S7 Client: Wrote and Read Value {read_data[0]}\")
         time.sleep(5)
 finally:
     client.disconnect()
-\" &" &
+" &' &
+echo "Klient S7 uruchomiony"
 
 # Utrzymanie skryptu aktywnego
 wait
